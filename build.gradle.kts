655851a7ccc374650336abb5704153ce9180ec00
Almighty Alpaca <aljoschagrebe@gmail.com>
2019-09-09 14:03:28 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -265,6 +265,6 @@ tasks {
 
     withType<Wrapper> {
         distributionType = Wrapper.DistributionType.ALL
-        gradleVersion = "5.6"
+        gradleVersion = "5.6.2"
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("jvm") version "1.3.41"
+        kotlin("jvm") version "1.3.50"
         id("com.github.ben-manes.versions") version "0.22.0"
         id("com.github.johnrengelman.shadow") version "5.1.0"
         id("com.palantir.consistent-versions") version "1.10.0"
